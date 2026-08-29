fee200e1d9c0d54d8573faa47c298d4fa522a396
Eragon <168056469+Edward-Wonder-Mordey@users.noreply.github.com>
2026-08-29 14:26:49 +0000

    Use redesigned enterprise hero on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,10 @@
 import Header from "../components/Header";
-import HeroSection from "../components/HeroSection";
+import HeroSection from "../components/HeroSection.new";
 import ServicesSection from "../components/ServicesSection";
 import ProjectsSection from "../components/ProjectsSection";
 import AboutSection from "../components/AboutSection";
 import ContactSection from "../components/ContactSection";
 
 export default function HomePage() {
-  return (
-    <div className="min-h-screen bg-white dark:bg-[#121212]">
-      <Header />
-      <HeroSection />
-      <ServicesSection />
-      <ProjectsSection />
-      <AboutSection />
-      <ContactSection />
-    </div>
-  );
+  return <div className="min-h-screen bg-white dark:bg-slate-950"><Header /><HeroSection /><ServicesSection /><ProjectsSection /><AboutSection /><ContactSection /></div>;
 }
